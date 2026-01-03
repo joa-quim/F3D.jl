36f151e2995af1b8d8a0e9f2cf492a9c40033a0d
Joaquim <jluis@ualg.pt>
2026-01-03 18:25:38 +0000

    Remove a needless ref to Windows and ad a few instructions more.

--- src/F3D.jl
+++ src/F3D.jl
@@ -4,11 +4,10 @@ module F3D
 const F3D_BIN_DIR = "C:/programs/compa_libs/F3D/bin"
 #const F3D_BIN_DIR = "C:/programs/compa_libs/f3d-superbuild/build/install/bin"
 
-# Add bin directory to PATH so dependent DLLs can be found (Windows only)
-if Sys.iswindows()
-    ENV["PATH"] = F3D_BIN_DIR * ";" * get(ENV, "PATH", "")
-end
+# Add bin directory to PATH so dependent DLLs/Shared libs can be found
+ENV["PATH"] = F3D_BIN_DIR * ";" * get(ENV, "PATH", "")
 
+# This for windows. For other SOs, change the extension accordingly
 const libf3d = joinpath(F3D_BIN_DIR, "f3d_c_api.dll")
 
 include("libf3d.jl")
